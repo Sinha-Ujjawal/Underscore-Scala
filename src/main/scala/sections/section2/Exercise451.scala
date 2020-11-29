de76c7ff93f46fa6dc1ded90e0b315423de98832
Sinha,Ujjawal <ujjawalsinhacool16021998@gmail.com>
2020-11-29 21:04:54 +0530

    Add Section 2 / Exercise 4 / 51

---
+++ src/main/scala/sections/section2/Exercise451.scala
@@ -0,0 +1,18 @@
+package sections.section2
+
+object Exercise451 extends App {
+  object Oswald {
+    val color = "Black"
+    val food = "Milk"
+  }
+
+  object Henderson {
+    val color = "Ginger"
+    val food = "Chips"
+  }
+
+  object Quentin {
+    val color = "Tabby and white"
+    val food = "Curry"
+  }
+}
